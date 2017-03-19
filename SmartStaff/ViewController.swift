2b021bdbeff1321d0fc41b973cbac6e694facefa
Nguyen Duy Khanh <dLn-wN7-GMe-d9pkhanhnd@runsystem.net>
2017-03-19 21:08:50 +0700

    first commit

---
+++ SmartStaff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartStaff
+//
+//  Created by Nguyen Duy Khanh on 3/17/17.
+//  Copyright © 2017 Nguyen Duy Khanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
